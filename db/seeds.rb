0d713d878f3224f7daa77f671bbf1d3e00e7c712
Emily Chuah <emily_earn@live.com>
2022-02-26 23:27:01 +1100

    Seed the movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "json"
+require "open-uri"
+
+puts "Cleaning database..."
+Movie.destroy_all if Rails.env == 'development'
+
+puts "Creating movies..."
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+top_20_movies = movies["results"].first(20)
+
+top_20_movies.each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Finished!"
