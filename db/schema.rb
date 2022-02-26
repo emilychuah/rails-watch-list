7a82ef10e7568230e21a8f44301b9d708cd54373
Emily Chuah <emily_earn@live.com>
2022-02-26 22:53:38 +1100

    Created models for movie, list and bookmark

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_054115) do
+ActiveRecord::Schema.define(version: 2022_02_26_115301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 2022_02_24_054115) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "overview"
+    t.text "overview"
     t.string "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
