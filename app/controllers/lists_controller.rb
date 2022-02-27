f04e4fb29e5ca1bd96816f9a4a8db7518cb6a4c9
Emily Chuah <emily_earn@live.com>
2022-02-27 14:53:06 +1100

    Added styles, added a new table for reviews in schema, and created a new model and controller for Reviews.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -22,13 +24,15 @@ class ListsController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:id])
     @list.destroy
-
     redirect_to lists_path
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import "controllers"
+import "select2/dist/css/select2.min.css"
 
 Rails.start()
 Turbolinks.start()
